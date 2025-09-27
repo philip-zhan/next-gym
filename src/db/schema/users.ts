48b4c27388bc40fd8f7de08d945cb8028a81cf68
Philip Zhan <h6zhan@gmail.com>
2025-09-26 21:29:15 -0700

    user resolvers

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -1,7 +1,9 @@
-import { integer, pgTable } from "drizzle-orm/pg-core";
+import { integer, pgTable, text } from "drizzle-orm/pg-core";
 import { timestamps } from "./_timestamps";
 
 export const users = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: text().notNull(),
+  email: text().notNull().unique(),
   ...timestamps,
 });
